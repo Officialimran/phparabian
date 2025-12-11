23b42344424d704b6698dea876366bce3fc3719e
Imran Ahmed <imran.softwindtech@gmail.com>
2025-12-11 14:53:15 +0600

    landing page added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+          poppins: ["Poppins"],
+          objectsans: ['Object Sans', 'sans-serif'],
+          roboto: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
